52d86e990b3f1698465def224af77c84890b4257
namanshrimali <35870572+namanshrimali@users.noreply.github.com>
2021-03-20 23:10:46 +0530

    Update detect.py

--- detect.py
+++ detect.py
@@ -164,7 +164,7 @@ def detect(save_img=False):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--cfg', type=str, default='cfg/yolov3-custom.cfg', help='*.cfg path')
-    parser.add_argument('--names', type=str, default='data/customdata/custom.names', help='*.names path')
+    parser.add_argument('--names', type=str, default='data/assignment13/custom.names', help='*.names path')
     parser.add_argument('--weights', type=str, default='weights/last.pt', help='weights path')
     parser.add_argument('--source', type=str, default='data/customdata/images', help='source')  # input file/folder, 0 for webcam
     parser.add_argument('--output', type=str, default='output', help='output folder')  # output folder
